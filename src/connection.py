52e9864574bd0c73175022586d3c4a8fee36640a
mel-py <melissa.buljubasic@gmail.com>
2019-03-04 17:13:37 -0700

    added post and delete functions

--- src/connection.py
+++ src/connection.py
@@ -1,8 +1,7 @@
 #!flask/bin/python
 
 import sqlite3
-from flask import Flask, jsonify, abort, make_response
-
+from flask import Flask, jsonify, abort, make_response, request
 app = Flask(__name__)
 
 posts = [
@@ -46,7 +45,24 @@ def get_post(post_id):
 		abort(404)
 	return jsonify({'post': post[0]})
 	
-
+@app.route('/chatter/api/v1.0/posts', methods=['POST'])
+def create_task():
+	if not request.json or not 'body' in request.json:
+		abort(400)
+	post={
+		"id": posts[-1]['id'] + 1,
+		"body": request.json['body']
+	}
+	posts.append(post)
+	return jsonify({'post': post}), 201
+	
+@app.route('/chatter/api/v1.0/posts/<int:post_id>', methods=['DELETE'])
+def delete_post(post_id):
+	post = [post for post in posts if post['id'] == post_id]
+	if len(post) == 0:
+		abort(404)
+	posts.remove(post[0])
+	return jsonify({'result': True})
 
 if __name__ == '__main__':
 	#conn = create_connection("C:\Programming\chatter\chatter_db.sqlite")
