b6a5d06f295defb41defaf596d8d686217df7aba
mel-py <melissa.buljubasic@gmail.com>
2019-03-09 10:00:59 -0700

    Save posts to database so they can be reloaded later

--- src/connection.py
+++ src/connection.py
@@ -5,19 +5,8 @@ from flask import Flask, jsonify, abort, make_response, request
 from flask_cors import CORS
 
 app = Flask(__name__)
+posts = []
 
-posts = [
-	{
-		"id": 1,
-		"body": "Hello world"
-	},
-	{
-		"id": 2,
-		"body": "I am glad I didn't sleep in today"
-	}
-]
-
-'''
 def create_connection(db_file):
 	try:
 		conn = sqlite3.connect(db_file)
@@ -26,16 +15,26 @@ def create_connection(db_file):
 		print(e)
 
 	return None
-	'''
 	
-'''def execute_query(conn, query):
-	try:
-		c = conn.cursor()
-		c.execute(query)
-	except Error as e:
-		print(e)
-'''
-
+def execute_query_fetch(query):
+	conn = create_connection("C:\Programming\chatter\chatter_db.sqlite")
+	c = conn.cursor()
+	c.execute(query)
+		
+	row = c.fetchone()
+	while row is not None:
+		print(row)
+		row = c.fetchone()
+	conn.commit()
+	conn.close()
+	
+def execute_query(query):
+	conn = create_connection("C:\Programming\chatter\chatter_db.sqlite")
+	c = conn.cursor()
+	c.execute(query)
+	conn.commit()
+	conn.close()
+	
 @app.after_request
 def after_request(response):
   response.headers.add('Access-Control-Allow-Origin', '*')
@@ -55,7 +54,7 @@ def get_post(post_id):
 	return jsonify({'post': post[0]})
 	
 @app.route('/chatter/api/v1.0/posts', methods=['POST'])
-def create_task():
+def create_post():
 	if not request.json or not 'body' in request.json:
 		abort(400)
 	post={
@@ -63,6 +62,9 @@ def create_task():
 		"body": request.json['body']
 	}
 	posts.append(post)
+	#INSERT INTO posts VALUES (id, body);
+	query = "INSERT INTO posts VALUES (" + str(post['id']) + ", \"" + post['body'] + "\");"
+	execute_query(query)
 	return jsonify({'post': post}), 201
 	
 @app.route('/chatter/api/v1.0/posts/<int:post_id>', methods=['DELETE'])
@@ -72,8 +74,26 @@ def delete_post(post_id):
 		abort(404)
 	posts.remove(post[0])
 	return jsonify({'result': True})
+	
+#Load all the data from the database into the API
+#TODO: Make table better (needs keys and stuff)
+def load_data():
+	c = conn.cursor()
+	c.execute("SELECT * FROM posts;");
+	
+	row = c.fetchone()
+	while row is not None:
+		post = {
+			"id": row[0],
+			"body": row[1]
+		}
+		posts.append(post)
+		row = c.fetchone()
 
 if __name__ == '__main__':
-	#conn = create_connection("C:\Programming\chatter\chatter_db.sqlite")
+	conn = create_connection("C:\Programming\chatter\chatter_db.sqlite")
+	load_data()
+	conn.commit()
+	conn.close()
 	
 	app.run(debug=True)
